6f46783519745ae8dd51463ff9ea1f10e5a25e2e
OsmanMikael <s374196@oslomet.no>
2025-04-09 14:24:18 +0200

    oppdater firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -11,13 +11,13 @@ import {
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSR0GVJvnGgi2y2tvHgqs2-75-pBZxS-w",
-  authDomain: "nor-senter.firebaseapp.com",
-  projectId: "nor-senter",
-  storageBucket: "nor-senter.firebasestorage.app",
-  messagingSenderId: "181059608560",
-  appId: "1:181059608560:web:f5076fa3c5111a4acd0735",
-  measurementId: "G-CKQ54Y34CY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialiser Firebase
