7a3d0799e98dc5f320d865c8ae4269e27d661cd9
OsmanMikael <s374196@oslomet.no>
2025-04-25 22:51:46 +0200

    oppretelse av sidebar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import PrayerTimes from "./PrayerTimes";
-import { FaWhatsapp } from "react-icons/fa"; // Importer WhatsApp-ikonet
+import { FaWhatsapp } from "react-icons/fa";
 import Donations from "./Donations";
 import Medier from "./Media";
-import { getAllNews, saveNews, deleteNews } from "./firebase"; // Importer Firebase-funksjoner
+import { getAllNews, saveNews, deleteNews } from "./firebase";
 
 const Home = () => {
   const { isAdmin } = useAuth();
-  const whatsappNumber = "+4792506096"; // Erstatt med riktig nummer
+  const whatsappNumber = "+4792506096";
 
   const [news, setNews] = useState([]);
   const [title, setTitle] = useState("");
@@ -16,9 +16,8 @@ const Home = () => {
   const [content, setContent] = useState("");
   const [editMode, setEditMode] = useState(false);
   const [editItem, setEditItem] = useState(null);
-  const [loading, setLoading] = useState(false); // Loading state
+  const [loading, setLoading] = useState(false);
 
-  // Hent nyheter fra Firestore ved montering
   useEffect(() => {
     const fetchNews = async () => {
       try {
@@ -33,7 +32,7 @@ const Home = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true); // Sett loading state til true
+    setLoading(true);
     let updatedNews;
 
     try {
@@ -43,23 +42,16 @@ const Home = () => {
         );
         await saveNews({ id: editItem.id, title, date, content });
       } else {
-        const newNews = {
-          title,
-          date,
-          content,
-        };
+        const newNews = { title, date, content };
         updatedNews = [...news, newNews];
         const result = await saveNews(newNews);
-        if (result.success) {
-          newNews.id = result.id; // Sett den genererte ID-en
-        }
+        if (result.success) newNews.id = result.id;
       }
-
       setNews(updatedNews);
     } catch (error) {
       console.error("Error saving news:", error);
     } finally {
-      setLoading(false); // Sett loading state til false
+      setLoading(false);
     }
 
     setEditMode(false);
@@ -78,7 +70,7 @@ const Home = () => {
   };
 
   const handleDelete = async (id) => {
-    setLoading(true); // Sett loading state til true
+    setLoading(true);
     try {
       await deleteNews(id);
       const updatedNews = news.filter((item) => item.id !== id);
@@ -86,116 +78,119 @@ const Home = () => {
     } catch (error) {
       console.error("Error deleting news:", error);
     } finally {
-      setLoading(false); // Sett loading state til false
+      setLoading(false);
     }
   };
 
   return (
-    <div className="containerH mt-4">
-      <h2 className="velkommen">Velkommen til Nor Senter</h2>
-      <PrayerTimes />
-      <Medier />
+    <div className="home-layout">
+      <aside className="sidebar">
+        <Donations />
+      </aside>
+      <div className="main-content">
+        <h2 className="velkommen">Velkommen til Nor Senter</h2>
+        <PrayerTimes />
+        <Medier />
 
-      <section className="news mb-4">
-        <h3>Nyheter og kunngjøringer</h3>
-        {isAdmin && (
-          <form onSubmit={handleSubmit} className="mb-4">
-            <div className="form-group">
-              <label htmlFor="title">Tittel:</label>
-              <input
-                type="text"
-                id="title"
-                className="form-control"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="date">Dato:</label>
-              <input
-                type="date"
-                id="date"
-                className="form-control"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="content">Kommentar:</label>
-              <textarea
-                id="content"
-                className="form-control"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              disabled={loading}
-            >
-              {editMode && editItem ? "Oppdater nyhet" : "Legg til nyhet"}
-            </button>
-            {loading && (
-              <div className="spinner-border mt-2" role="status">
-                <span className="sr-only">Laster...</span>
+        <section className="news mb-4">
+          <h3>Nyheter og kunngjøringer</h3>
+          {isAdmin && (
+            <form onSubmit={handleSubmit} className="mb-4">
+              <div className="form-group">
+                <label htmlFor="title">Tittel:</label>
+                <input
+                  type="text"
+                  id="title"
+                  className="form-control"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                />
               </div>
-            )}{" "}
-            {/* Vis en lastemelding */}
-          </form>
-        )}
-        <div className="row">
-          {news.map((item) => (
-            <div key={item.id} className="col-md-4 mb-4">
-              <div className="card">
-                <div className="card-body">
-                  <h4 className="card-title">{item.title}</h4>
-                  <p className="card-text">
-                    <strong>Dato:</strong> {item.date}
-                  </p>
-                  <p className="card-text">{item.content}</p>
-                  {isAdmin && (
-                    <>
-                      <button
-                        className="btn btn-secondary mr-2"
-                        onClick={() => handleEdit(item)}
-                      >
-                        Rediger
-                      </button>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => handleDelete(item.id)}
-                      >
-                        Slett
-                      </button>
-                    </>
-                  )}
+              <div className="form-group">
+                <label htmlFor="date">Dato:</label>
+                <input
+                  type="date"
+                  id="date"
+                  className="form-control"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="content">Kommentar:</label>
+                <textarea
+                  id="content"
+                  className="form-control"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={loading}
+              >
+                {editMode && editItem ? "Oppdater nyhet" : "Legg til nyhet"}
+              </button>
+              {loading && (
+                <div className="spinner-border mt-2" role="status">
+                  <span className="sr-only">Laster...</span>
+                </div>
+              )}
+            </form>
+          )}
+          <div className="row">
+            {news.map((item) => (
+              <div key={item.id} className="col-md-4 mb-4">
+                <div className="card">
+                  <div className="card-body">
+                    <h4 className="card-title">{item.title}</h4>
+                    <p className="card-text">
+                      <strong>Dato:</strong> {item.date}
+                    </p>
+                    <p className="card-text">{item.content}</p>
+                    {isAdmin && (
+                      <>
+                        <button
+                          className="btn btn-secondary mr-2"
+                          onClick={() => handleEdit(item)}
+                        >
+                          Rediger
+                        </button>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => handleDelete(item.id)}
+                        >
+                          Slett
+                        </button>
+                      </>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
 
-      <section className="location">
-        <h3>Lokasjon</h3>
-        <div className="map-container">
-          <iframe
-            title="Moske Lokasjon"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21345.148973143268!2d10.733456!3d59.911491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e8b2e4e8b:0x8e2b2ad6!2sNedre%20Rommen%207a,%200988%20Oslo,%20Norge!5e0!3m2!1sen!2sno!4v1638612345678!5m2!1sen!2sno"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-          ></iframe>
-        </div>
-      </section>
-      <Donations />
-      {/* WhatsApp-knapp */}
+        <section className="location">
+          <h3>Lokasjon</h3>
+          <div className="map-container">
+            <iframe
+              title="Moske Lokasjon"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21345.148973143268!2d10.733456!3d59.911491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e8b2e4e8b:0x8e2b2ad6!2sNedre%20Rommen%207a,%200988%20Oslo,%20Norge!5e0!3m2!1sen!2sno!4v1638612345678!5m2!1sen!2sno"
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        </section>
+      </div>
+
       <a
         href={`https://wa.me/${whatsappNumber}`}
         target="_blank"
